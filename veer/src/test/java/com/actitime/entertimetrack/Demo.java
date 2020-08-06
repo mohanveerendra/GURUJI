139f5e14933351e6654ec593ee92047f99190f6d
dell <dell@DESKTOP-0KC6S3A>
2020-08-06 20:58:55 +0530

    my project commit

---
+++ veer/src/test/java/com/actitime/entertimetrack/Demo.java
@@ -0,0 +1,11 @@
+package com.actitime.entertimetrack;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+@Test
+public void massmaharaj() {
+	Reporter.log("die hard fan of massmaharaj",true);
+}
+}
